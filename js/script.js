45fb2ec8928a0d70ecbe58a100ce4b17e2c1e95f
ryanrss <ryanrunakoo@hotmail.com>
2024-05-20 04:08:08 +0700

    header
    add header 100%
    luas dan keliling function 50%

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert("test");
+const hitungLuas = (tipe) => {
+  let alas = document.getElementById("alas").value;
+  let tinggi = document.getElementById("tinggi").value;
+  if (!Number(alas) || !Number(tinggi)) {
+    console.log("invalid input ");
+    return alert("INVALID INPUT!!! please input numeric chars");
+  }
+  let result = (1 / 2) * alas * tinggi;
+  console.log(result);
+};
